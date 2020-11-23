8ea099502054034dd0b37228b5bdbe819086f617
biquet <303204@supinfo.com>
2020-11-23 15:09:24 +0100

    Ouverture html avec navigateur

--- MeteoInMadrid.py
+++ MeteoInMadrid.py
@@ -1,17 +1,21 @@
 import pandas as pd
 import numpy as np
 import seaborn as sb
-from matplotlib import matplotlib.pylot as mp
+import matplotlib as mp
+import webbrowser
 
 
 
 
-#Initialisation#
+#Initialisation
 def main():
-    data = pd.read_csv(r'weather_madrid.csv', sep=',', index_col=0)
-    
+    data = pd.read_csv('D:\\Documents\\Supinfo\\Cours\\Asc2\\2_DTTL\Projet_Final\\2DTTL\\weather_madrid.csv', sep=',', index_col=0)
+    myDataFrame = pd.DataFrame(data)    
+    fichier = open("D:\\Documents\\Supinfo\\Cours\\Asc2\\2_DTTL\Projet_Final\\2DTTL\\index.html", "a")
+    fichier.write(myDataFrame.to_html(max_rows=10))
+    fichier.close
+    webbrowser.open("D:\\Documents\\Supinfo\\Cours\\Asc2\\2_DTTL\Projet_Final\\2DTTL\\index.html")
 
 
-def main():
-    
+main()
 
