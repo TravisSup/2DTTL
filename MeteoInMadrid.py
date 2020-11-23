c9b02fd57fd238cb3e325143a4cb6348fc4f4baa
biquet <303204@supinfo.com>
2020-11-23 14:16:23 +0100

    ajout csv

---
+++ MeteoInMadrid.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+import seaborn as sb
+from matplotlib import matplotlib.pylot as mp
+
+
+
+
+#Initialisation#
+def main():
+    data = pd.read_csv(r'weather_madrid.csv', sep=',', index_col=0)
+    
+
+
+def main():
+    
+
